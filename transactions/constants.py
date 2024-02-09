87d1e4ed39f55a921fec22d8c7ec2ad20052c255
Ishraq Sikder <114902077+IshraqSikder@users.noreply.github.com>
2024-02-09 11:46:35 +0600

    Add files via upload

---
+++ transactions/constants.py
@@ -0,0 +1,16 @@
+# arekta niyom.. ager niyomeo kora jabe
+DEPOSIT = 1
+WITHDRAWAL = 2
+LOAN = 3
+LOAN_PAID = 4
+SEND = 5
+RECIEVE = 6
+
+TRANSACTION_TYPE = (
+    (DEPOSIT, 'Deposit'),
+    (WITHDRAWAL, 'Withdrawal'),
+    (LOAN, 'Loan'),
+    (LOAN_PAID, 'Loan Paid'),
+    (SEND, 'Send'),
+    (RECIEVE, 'Recieve'),
+)
